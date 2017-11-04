eba5643aad416e3475f58fb57a01cc4b83a20ff4
Judy Jinn <judyjinn@gmail.com>
2017-11-03 20:03:01 -0700

    Working code to gather all nodes DAM hourly data

--- data/pyiso_node_scraper.py
+++ data/pyiso_node_scraper.py
@@ -5,41 +5,44 @@ Pyiso script for gathering node data
 '''
 import time as timeit
 import os
+import numpy as np
 import pandas as pd
 pd.set_option('display.max_columns',0)
-from CAISOClient import *
+# from CAISOClient import *
+from pyiso import caiso
+from datetime import datetime, timedelta, time
 import os
+
 path=os.getcwd()
 
-caiso = CAISOClient()
+caiso_data = caiso.CAISOClient()
+caiso_data.timeout_seconds = 120
 
-starttime = caiso.utcify('2017-10-01 00:00')
-endtime = caiso.utcify('2017-10-31 23:00')
-now = caiso.utcify(datetime.utcnow(), tz_name='utc')
+starttime = caiso_data.utcify('2017-10-01 00:00')
+endtime = caiso_data.utcify('2017-10-01 23:00')
+now = caiso_data.utcify(datetime.utcnow(), tz_name='utc')
 
-options = {
-    'start_at': starttime,
-    'end_at': endtime,
-    'freq': '1hr',
-    'market': 'DAM'
-    }
-    
-caiso.options = options
+caiso_data.handle_options(freq='1hr', market='DAM')
 
 # # Get node locations
 # LMP_locs = pd.DataFrame(caiso.get_lmp_loc())
 # LMP_locs=LMP_locs[LMP_locs['area']=='CA']
 # LMP_locs.to_csv('LMP_locs.csv', sep=',', index=False)
 LMP_locs = pd.read_csv('LMP_locs.csv', sep=',')
-node_names = LMP_locs['node_id'].unique()
+node_names = list(LMP_locs['node_id'].unique())
 
-# # Demand Forecast
-time1 = timeit.time()
+# Fuel Types
+fuel = pd.DataFrame(caiso_data._generation_historical())
+fuel.to_csv('fuel.csv', sep=',', index=False)
 
-node_id = node_names[0]
 
-lmp = caiso.get_lmp_as_dataframe(node_id, False, starttime, endtime)
-lmp.to_csv(path+'/node_LMP/'+ lmp['PNODE_RESMRID'][0]+'.csv', sep=',', index=False)
+# time1 = timeit.time()
+num_nodes = len(node_names)
+# # LMP DAMs
+for node_id in node_names:
+    print(num_nodes-node_names.index(node_id), 'nodes left')
+    lmp = caiso_data.get_lmp_as_dataframe(node_id, False, starttime, endtime, freq='1hr', market='DAM', market_run_id='DAM')
+    lmp.to_csv(path+'/node_LMP/'+ lmp['PNODE_RESMRID'][0]+'.csv', sep=',', index=False)
 
-tot_time = timeit.time() - time1
-print("Time taken: {:.2f} seconds".format(tot_time))
+# tot_time = timeit.time() - time1
+# print("Time taken: {:.2f} seconds".format(tot_time))
