1b0a77703d339f1432e0a68e8765888f95f953b9
Judy Jinn <judyjinn@gmail.com>
2017-11-03 20:13:04 -0700

    added code for fuel types, load, and trade

--- data/pyiso_node_scraper.py
+++ data/pyiso_node_scraper.py
@@ -19,7 +19,7 @@ caiso_data = caiso.CAISOClient()
 caiso_data.timeout_seconds = 120
 
 starttime = caiso_data.utcify('2017-10-01 00:00')
-endtime = caiso_data.utcify('2017-10-01 23:00')
+endtime = caiso_data.utcify('2017-10-21 23:00')
 now = caiso_data.utcify(datetime.utcnow(), tz_name='utc')
 
 caiso_data.handle_options(freq='1hr', market='DAM')
@@ -32,17 +32,24 @@ LMP_locs = pd.read_csv('LMP_locs.csv', sep=',')
 node_names = list(LMP_locs['node_id'].unique())
 
 # Fuel Types
-fuel = pd.DataFrame(caiso_data._generation_historical())
+fuel = pd.DataFrame(caiso_data.get_generation(False, False, starttime, endtime, freq='1hr'))
 fuel.to_csv('fuel.csv', sep=',', index=False)
 
 
-# time1 = timeit.time()
+load = pd.DataFrame(caiso_data.get_load(False, starttime, endtime, freq='1hr'))
+load.to_csv('load.csv', sep=',', index=False)
+
+trade = pd.DataFrame(caiso_data.get_trade(False, starttime, endtime, freq='1hr', market='DAM', market_run_id='DAM'))
+trade.to_csv('trade.csv', sep=',', index=False)
+
+time1 = timeit.time()
+
+# LMP DAMs
 num_nodes = len(node_names)
-# # LMP DAMs
 for node_id in node_names:
     print(num_nodes-node_names.index(node_id), 'nodes left')
     lmp = caiso_data.get_lmp_as_dataframe(node_id, False, starttime, endtime, freq='1hr', market='DAM', market_run_id='DAM')
     lmp.to_csv(path+'/node_LMP/'+ lmp['PNODE_RESMRID'][0]+'.csv', sep=',', index=False)
 
-# tot_time = timeit.time() - time1
-# print("Time taken: {:.2f} seconds".format(tot_time))
+tot_time = timeit.time() - time1
+print("Time taken: {:.2f} seconds".format(tot_time))
